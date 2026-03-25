9ae7f0d4611ffd1290c351e06b2008b3b02bfacb
ca3wilkes <ca3wilkes@gmail.com>
2026-03-25 20:51:02 +0000

    d

--- api/inventory.js
+++ api/inventory.js
@@ -9,19 +9,30 @@ export default async function handler(req, res) {
     const invetoryRes = await fetch(`https://bitjita.com/api/players/${id}/inventories`);
     const inventoryData  = await invetoryRes.json();
 
+    const inventoryObjects = inventoryData.inventories || [];
+
+    const mappedInven = inventoryObjects.inventories.map(inv =>({
+    id: inv.entityId,
+    name: inv.inventoryName,
+    itemCollection: inv.pockets.map(pockets =>({
+      itemid: pockets.contents.itemId,
+      itemInfo: inventoryData.items[pockets.contents.itemId],
+      quantit: pockets.contents.quantity
+    }))
+}));
 
 
     if (!mappedInven) {
       return res.status(404).json({ error: "Player not found." });
     }
 
-    
+    results = mappedInven;
 
     res.status(200).json({
-        inventory: inventoryData,
+        inventories: results
     });
 
   } catch (err) {
-    res.status(500).json({ error: "Failed to fetch Player data" });
+    res.status(500).json({ error: res.message });
   }
 }
