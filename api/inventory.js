b246e2baca926632edda5959f942239c88950bcf
ca3wilkes <ca3wilkes@gmail.com>
2026-03-25 20:39:18 +0000

    a

--- api/inventory.js
+++ api/inventory.js
@@ -11,16 +11,24 @@ export default async function handler(req, res) {
 
     const inventoryObjects = inventoryData.inventories || [];
 
-    
+    const mappedInven = inventoryObjects.inventories.map(inv =>({
+    id: inv.entityId,
+    name: inv.inventoryName,
+    itemCollection: inv.pockets.map(pockets =>({
+      itemid: pockets.contents.itemId,
+      itemInfo: inventoryData.items[pockets.contents.itemId],
+      quantit: pockets.contents.quantity
+    }))
+}));
 
 
-    if (!mappedInventories) {
+    if (!mappedInven) {
       return res.status(404).json({ error: "Player not found." });
     }
 
-    results = mappedInventories;
+    results = mappedInven;
 
-    res.json({
+    res.status(200).json({
         inventories: results
     });
 
