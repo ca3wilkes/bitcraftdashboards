8a6011133c4432fb333016c3367add1d5a60d2c4
ca3wilkes <ca3wilkes@gmail.com>
2026-03-25 21:38:21 +0000

    bugfix

--- api/inventory.js
+++ api/inventory.js
@@ -15,7 +15,7 @@ export default async function handler(req, res) {
     name: inv.inventoryName,
     itemCollection: inv.pockets.map(pockets =>({
       itemid: pockets.contents.itemId,
-      itemInfo: inventoryData.items[pockets.contents.itemId] || inventoryData.cargos[pockets.contents.itemId] || null,
+      itemInfo: pockets.itemtype == 0? inventoryData.items[pockets.contents.itemId] : inventoryData.cargos[pockets.contents.itemId],
       quantit: pockets.contents.quantity
     }))
 }));
